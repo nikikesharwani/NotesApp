ca0bd26e7de974760863551072917ab7b413e0dc
Nikita Kesharwani <nikitakesharwani09.nk@gmail.com>
2019-09-09 22:23:25 +0530

    modified server.js

--- src/server.js
+++ src/server.js
@@ -5,23 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist'));
-
-const forceSSL = function() {
-    return function (req, res, next) {
-        if (req.headers['x-forwarded-proto'] !== 'https') {
-        return res.redirect(
-            ['https://', req.get('Host'), req.url].join('')
-        );
-        }
-        next();
-    }
-}
-
-app.use(forceSSL());
+app.use(express.static(__dirname, 'dist'));
 
 app.get('/*', function(req,res) {  
-    res.sendFile(path.join(__dirname+'/dist/index.html'));
+    res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8070);
